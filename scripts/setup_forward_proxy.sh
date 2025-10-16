1e6079fd894168efa9bf84c8282b901e52041fc1
Ubuntu <ubuntu@web1.test.com>
2025-10-16 09:18:23 +0000

    Updated script

--- scripts/setup_forward_proxy.sh
+++ scripts/setup_forward_proxy.sh
@@ -5,7 +5,7 @@ sudo apt install nginx -y
 sudo systemctl enable nginx
 sudo systemctl start nginx
 
-sudo cp ~/nginx-proxy-lb-demo/nginx/forward-proxy.conf /etc/nginx/sites-available/
+sudo cp ~/Nginx-LB-Proxy/nginx/forward-proxy.conf /etc/nginx/sites-available/
 sudo ln -sf /etc/nginx/sites-available/forward-proxy.conf /etc/nginx/sites-enabled/
 
 sudo nginx -t && sudo systemctl restart nginx
